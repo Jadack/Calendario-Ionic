b3a118306cf80fa3f98c249b593239e7c2e83b3e
Jack Damiolix Pacheco Luciano <jackdamiolixp@gmail.com>
2018-07-01 17:10:46 -0400

    Add files via upload

---
+++ www/build/0.js
@@ -0,0 +1,90 @@
+webpackJsonp([0],{
+
+/***/ 406:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "EventModalPageModule", function() { return EventModalPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(21);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__event_modal__ = __webpack_require__(407);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var EventModalPageModule = /** @class */ (function () {
+    function EventModalPageModule() {
+    }
+    EventModalPageModule = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["I" /* NgModule */])({
+            declarations: [
+                __WEBPACK_IMPORTED_MODULE_2__event_modal__["a" /* EventModalPage */],
+            ],
+            imports: [
+                __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__event_modal__["a" /* EventModalPage */]),
+            ],
+        })
+    ], EventModalPageModule);
+    return EventModalPageModule;
+}());
+
+//# sourceMappingURL=event-modal.module.js.map
+
+/***/ }),
+
+/***/ 407:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EventModalPage; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(21);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_moment__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_moment___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_moment__);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var EventModalPage = /** @class */ (function () {
+    function EventModalPage(navCtrl, navParams, viewCtrl) {
+        this.navCtrl = navCtrl;
+        this.navParams = navParams;
+        this.viewCtrl = viewCtrl;
+        this.event = { startTime: new Date().toISOString(), endTime: new Date().toISOString(), allDay: false };
+        this.minDate = new Date().toISOString();
+        var preselectedDate = __WEBPACK_IMPORTED_MODULE_2_moment__(this.navParams.get('selectedDay')).format();
+        this.event.startTime = preselectedDate;
+        this.event.endTime = preselectedDate;
+    }
+    EventModalPage.prototype.save = function () {
+        this.viewCtrl.dismiss(this.event);
+    };
+    EventModalPage = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+            selector: 'page-event-modal',template:/*ion-inline-start:"C:\Users\JackDamiolixPachecoL\Desktop\Black-Jack\Jack\Mobile Ionic\calendar\src\pages\event-modal\event-modal.html"*/'<!--\n  Generated template for the EventModalPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>Events Details</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content>\n  <ion-list>\n    <ion-item>\n      <ion-input type="text" placeholder="Title" [(ngModel)]="event.title"></ion-input>\n    </ion-item>\n    <ion-item>\n      <ion-label>Start</ion-label>\n      <ion-input displayFormat="MM//DD//YY HH:mm" pickerFormat="MM//DD//YY HH:mm" type="text" placeholder="Title" [(ngModel)]="event.startTime" [min]="minDate"></ion-input>\n    </ion-item>\n    <ion-item>\n      <ion-label>End</ion-label>\n      <ion-input displayFormat="MM//DD//YY HH:mm" pickerFormat="MM//DD//YY HH:mm" type="text" placeholder="Title" [(ngModel)]="event.endTime" [min]="minDate"></ion-input>\n    </ion-item>\n    <ion-item>\n      <ion-label>All Day?</ion-label>\n        <ion-checkbox [(ngModel)]="event.allDay"></ion-checkbox>\n      </ion-item>\n  </ion-list>\n  <button ion-button full icon-left (click)="save()">\n    <ion-icon name="checkmark"></ion-icon> Add Event\n  </button>\n</ion-content>\n'/*ion-inline-end:"C:\Users\JackDamiolixPachecoL\Desktop\Black-Jack\Jack\Mobile Ionic\calendar\src\pages\event-modal\event-modal.html"*/,
+        }),
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavParams */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["k" /* ViewController */]])
+    ], EventModalPage);
+    return EventModalPage;
+}());
+
+//# sourceMappingURL=event-modal.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=0.js.map
